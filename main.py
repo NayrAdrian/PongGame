383414d683980896bd0943468b1b3650d292c6b2
Ryan Adrian <NayrAdrian24@gmail.com>
2024-06-04 21:04:31 +0800

    added collision, bounce and reset position function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+import time
 import paddle
+from ball import Ball
 
 screen = Screen()
 screen.bgcolor("black")
@@ -10,15 +12,33 @@ screen.tracer(0)
 r_paddle = paddle.Paddle((350, 0))
 l_paddle = paddle.Paddle((-350, 0))
 
-
 screen.listen()
 screen.onkeypress(r_paddle.go_up, "Up")
 screen.onkeypress(r_paddle.go_down, "Down")
 screen.onkeypress(l_paddle.go_up, "w")
 screen.onkeypress(l_paddle.go_down, "s")
 
+ball = Ball()
+
 game_is_on = True
 while game_is_on:
+    time.sleep(0.01)  # delay to slow down the game
     screen.update()
+    ball.move()
+
+    # Detect collision with top and bottom walls
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddles
+    if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or (ball.distance(l_paddle) < 50 and ball.xcor() < -320):
+        ball.bounce_x()
+
+    # Detect ball out of bounds
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 screen.exitonclick()
