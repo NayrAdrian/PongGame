c0b88f88b7a404389937d31c96c7b1dcdab0f5c8
Ryan Adrian <NayrAdrian24@gmail.com>
2024-06-04 20:23:12 +0800

    Created paddle and movement function

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
-from turtle import Screen
+from turtle import Screen, Turtle
+import paddle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+screen.tracer(0)
+
+r_paddle = paddle.Paddle((350, 0))
+l_paddle = paddle.Paddle((-350, 0))
 
 
+screen.listen()
+screen.onkeypress(r_paddle.go_up, "Up")
+screen.onkeypress(r_paddle.go_down, "Down")
+screen.onkeypress(l_paddle.go_up, "w")
+screen.onkeypress(l_paddle.go_down, "s")
 
-
-
-
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 screen.exitonclick()
